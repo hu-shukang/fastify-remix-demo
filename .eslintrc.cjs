b4dea5d74304ee288628fea7d4c3cc3fe859b8fc
HU SHUKANG <hushukang@gmail.com>
2024-10-01 08:52:17 +0900

    add eslint&prettier settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node', 'plugin:prettier/recommended'],
   root: true,
 };
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
-import process from "node:process";
-import { remixFastify } from "@mcansh/remix-fastify";
-import { fastify } from "fastify";
-import sourceMapSupport from "source-map-support";
+import process from 'node:process';
+import { remixFastify } from '@mcansh/remix-fastify';
+import { fastify } from 'fastify';
+import sourceMapSupport from 'source-map-support';
 
 sourceMapSupport.install();
 
@@ -9,7 +9,7 @@ const app = fastify();
 
 await app.register(remixFastify);
 
-const host = process.env.HOST === "true" ? "0.0.0.0" : "127.0.0.1";
+const host = process.env.HOST === 'true' ? '0.0.0.0' : '127.0.0.1';
 const desiredPort = Number(process.env.PORT) || 3000;
 let address = await app.listen({ port: desiredPort, host });
 
