c71d1fca85769fe0958592c30404f0268713a4a2
胡书康 <hushukang@gmail.com>
2024-10-03 20:05:21 +0900

    add apigateway

--- cdk/lib/lambda-stack.ts
+++ cdk/lib/lambda-stack.ts
@@ -3,6 +3,7 @@ import type { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class LambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, envs: Record<string, string>, props?: cdk.StackProps) {
@@ -16,20 +17,16 @@ export class LambdaStack extends cdk.Stack {
     const assetBucketArn = cdk.Fn.importValue(`${envs.ASSET_BUCKET}-arn`);
     const assetBucket = s3.Bucket.fromBucketArn(this, envs.ASSET_BUCKET, assetBucketArn);
 
-    const layerCode = lambda.Code.fromBucket(assetBucket, 'layer.zip');
-    const layerS3Location = layerCode.bind(this).s3Location;
     const layer = new lambda.LayerVersion(this, `${envs.APP_NAME}-layer-${envs.ENV}`, {
       layerVersionName: `${envs.APP_NAME}-layer-${envs.ENV}`,
       compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
-      code: layerCode,
+      code: lambda.Code.fromBucket(assetBucket, 'layer.zip'),
     });
 
-    const serverLambdaCode = lambda.Code.fromBucket(assetBucket, 'server.zip');
-    const serverLambdaS3Location = serverLambdaCode.bind(this).s3Location;
-    new lambda.Function(this, `${envs.APP_NAME}-server-${envs.ENV}`, {
+    const serverLambda = new lambda.Function(this, `${envs.APP_NAME}-server-${envs.ENV}`, {
       functionName: `${envs.APP_NAME}-server-${envs.ENV}`,
       description: `${envs.APP_NAME}-server-${envs.ENV}`,
-      code: serverLambdaCode,
+      code: lambda.Code.fromBucket(assetBucket, 'server.zip'),
       runtime: lambda.Runtime.NODEJS_20_X,
       handler: 'lambda.handler',
       layers: [layer],
@@ -39,14 +36,12 @@ export class LambdaStack extends cdk.Stack {
       memorySize: 512,
     });
 
-    new cdk.CfnOutput(this, `${envs.APP_NAME}-layer-${envs.ENV}-s3-location`, {
-      value: `s3://${layerS3Location?.bucketName}/${layerS3Location?.objectKey}`,
-      exportName: `${envs.APP_NAME}-layer-${envs.ENV}-s3-location`,
-    });
-
-    new cdk.CfnOutput(this, `${envs.APP_NAME}-server-lambda-${envs.ENV}-s3-location`, {
-      value: `s3://${serverLambdaS3Location?.bucketName}/${serverLambdaS3Location?.objectKey}`,
-      exportName: `${envs.APP_NAME}-server-lambda-${envs.ENV}-s3-location`,
+    // 创建 API Gateway
+    new apigateway.LambdaRestApi(this, `${envs.APP_NAME}-api-${envs.ENV}`, {
+      restApiName: `${envs.APP_NAME}-api-${envs.ENV}`,
+      handler: serverLambda,
+      proxy: true,
+      deployOptions: { stageName: envs.ENV },
     });
   }
 }
