db221f1f09ff74d700eada8bffb60e83b5bd389f
胡书康 <hushukang@gmail.com>
2024-10-04 14:29:38 +0900

    change fastify app

--- server/index.ts
+++ server/index.ts
@@ -7,11 +7,15 @@ sourceMapSupport.install();
 
 export const app = fastify();
 
-await app.register(remixFastify);
+app.register(remixFastify);
 
 if (process.env.ENV === 'local') {
   const host = process.env.HOST === 'true' ? '0.0.0.0' : '127.0.0.1';
   const desiredPort = Number(process.env.PORT) || 3000;
-  let address = await app.listen({ port: desiredPort, host });
-  console.log(`app ready: ${address}`);
+  app
+    .listen({ port: desiredPort, host })
+    .then((address) => {
+      console.log(`app ready: ${address}`);
+    })
+    .catch(console.error);
 }
