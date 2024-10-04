0f7faca322bbad99e444053522a61e67f6b05e24
胡书康 <hushukang@gmail.com>
2024-10-04 17:54:33 +0900

    remove remix

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,14 @@
 import process from 'node:process';
-import { remixFastify } from '@mcansh/remix-fastify';
+// import { remixFastify } from '@mcansh/remix-fastify';
 import { fastify } from 'fastify';
 
 export const app = fastify();
 
-await app.register(remixFastify);
+// await app.register(remixFastify);
+
+app.get('/', async function handler(request, reply) {
+  return { hello: 'world' };
+});
 
 if (process.env.ENV === 'local') {
   const host = process.env.HOST === 'true' ? '0.0.0.0' : '127.0.0.1';
